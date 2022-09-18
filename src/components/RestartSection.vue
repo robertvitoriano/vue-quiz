3b2e6ca0a6a285ab2a2cc336f30a5be951db79e2
Robert Vitoriano <robertvitoriano@gmail.com>
2022-09-18 08:36:54 -0300

    add course finished section

--- src/components/RestartSection.vue
+++ src/components/RestartSection.vue
@@ -1,58 +1,76 @@
 <template>
-  <div class="restart-section-container">
-
-    <h1>{{performanceText}}</h1>
-    <h3>Do you want to try again ?</h3>
-    <div class='restart-buttons-container'>
-      <b-button class="restart-button" variant="danger" href="#">
-        finish course
-      </b-button>
-      <b-button class="restart-button" variant="success" href="#" @click="emitRestart">try again ?</b-button>
+    <div class="wrapper">
+      <div v-if="!courseFinished" class="restart-course-section">
+      <h1>{{performanceText}}</h1>
+      <h3>Do you want to try again ?</h3>
+      <div class='restart-buttons-container'>
+        <b-button class="restart-button" variant="danger" href="#" @click="finishCourse">
+          finish course
+        </b-button>
+        <b-button class="restart-button" variant="success" href="#" @click="emitRestart">try again ?</b-button>
+        </div>
+      </div>
+      <div v-if="courseFinished" class="course-finished-section">
+        <h1>This is it, I know you did your best!</h1>
+      </div>
     </div>
-  </div>
 </template>
 <script>
 export default {
   props: {
     score: Number
   },
-  data(){
-    return{
-      performanceText:''
+  data() {
+    return {
+      performanceText: '',
+      courseFinished: false
     }
   },
-  mounted(){
+  mounted() {
     this.performanceText = this.getPerformanceText()
   },
-  methods:{
-    emitRestart(){
+  methods: {
+    emitRestart() {
       this.$emit('restartEvent')
     },
-    getPerformanceText(){
-      if(this.score === 0) return 'Unfortunately you have not scored, try again, I know you can improve'
-      return `Congratulations, you obtained ${this.score} points !`  
-      
+    getPerformanceText() {
+      if (this.score === 0) return 'Unfortunately you have not scored, try again, I know you can improve'
+      return `Congratulations, you obtained ${this.score} points !`
+    },
+    finishCourse() {
+      this.courseFinished = true
     }
   }
 }
 
 </script>
 <style scoped>
+.restart-buttons-container {
+  width: 20%;
+  display: flex;
+  justify-content: space-between;
 
-  .restart-buttons-container{
-    width:20%;
-    display: flex;
-    justify-content: space-between;
+}
 
-  }
-.restart-section-container {
+.wrapper{
+  display: flex;
+  flex-direction: column;
+  height: 60vh;
+  background-color: gray;
+}
+.restart-course-section{
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  height:100vh;
-  background-color: gray;
-  height:60vh;
+  height: 100%;
 }
+.course-finished-section{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
 
+}
 </style>
