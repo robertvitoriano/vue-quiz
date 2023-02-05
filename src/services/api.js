08ec7c5b2a1c91a0733589caf00a081e118a2880
robertvitoriano <robertvitoriano@gmail.com>
2023-02-05 09:43:48 -0300

    add axios interceptors

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,37 @@
 import axios from 'axios'
+import userService from "./userService";
 
 const api = axios.create({
   baseURL:`${process.env.VUE_APP_API_URL}/api/v1`,
-  headers:{
-    authorization:"Bearer " + localStorage.getItem("token")
+});
+
+const requestIntercepter = (config) => {
+  config.headers.Authorization =
+  "Bearer " + localStorage.getItem("token");
+  return config;
+};
+const responseIntercepter = (response) => {
+  return response;
+};
+
+api.interceptors.request.use(requestIntercepter);
+
+
+api.interceptors.response.use(responseIntercepter, (err) => {
+  const authorizedUserError = err.response && err.response.status === 400 && err.response.status < 500;
+  const unauthorizedUserError = err.response.status === 401
+  if(authorizedUserError){
+    console.error(err);
   }
-})
+  if (unauthorizedUserError) {
+      userService.logout();
+      location.reload(true);
+    }
+
+    return Promise.reject(err);
+
+});
 
 export default api
+
+
